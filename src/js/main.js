2138f9d3f6395d865fef3399064e328c729ff387
simonruchet <simon.ruchet@studentfr.ch>
2025-09-25 17:10:14 +0200

    final

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,62 @@ document.addEventListener("DOMContentLoaded", function () {
   const exercice3 = document.querySelector("#exercice-3");
 
   if (exercice1) {
-    // Ecrire ici tout le code pour l'exercice 1: Sélecteurs JavaScript
+    document.querySelectorAll(".broccoli").forEach((el) => el.remove());
   }
 
   if (exercice2) {
-    // Ecrire ici tout le code pour l'exercice 2: Modification des classes
+    document.querySelector("#pizza-to-cook").classList.add("cooked");
+    document.querySelector(".burned").classList.remove("burned");
+    document.querySelector("#pizza-special").classList.toggle("highlighted");
+    document.querySelector(".basic").classList.add("premium", "deluxe");
+    document.querySelector(".reheated").classList.remove("cold");
+    document.querySelector(".reheated").classList.add("hot");
   }
 
   if (exercice3) {
-    // Ecrire ici tout le code pour l'exercice 3: Ecouteurs d'événement
+    const myElement = document.querySelector("#exercice-3-1");
+    const btn = document.querySelector("#exercice-3-1");
+
+    btn.addEventListener("click", function () {
+      myElement.classList.add("highlight");
+    });
+
+    const card2 = document.querySelector("#exercice-3-2");
+    const btn2 = card2.querySelector("button");
+
+    btn2.addEventListener("click", function () {
+      card2.classList.toggle("highlight");
+    });
+
+    const card3 = document.querySelector("#exercice-3-3");
+
+    card3.addEventListener("mouseenter", function () {
+      card3.classList.add("highlight");
+    });
+
+    card3.addEventListener("mouseleave", function () {
+      card3.classList.remove("highlight");
+    });
+
+    const card4 = document.querySelector("#exercice-3-4");
+
+    document.addEventListener("mousemove", function () {
+      card4.classList.add("highlight");
+    });
+    const card5 = document.querySelector("#exercice-3-5");
+
+    document.addEventListener("scroll", function () {
+      card5.classList.add("highlight");
+    });
+    const card6 = document.querySelector("#exercice-3-6");
+
+    document.addEventListener("keydown", function () {
+      card6.classList.add("highlight");
+    });
+    const card7 = document.querySelector("#exercice-3-7");
+
+    window.addEventListener("resize", function () {
+      card7.classList.add("highlight");
+    });
   }
 });
